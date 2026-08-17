7116f29157a93abd8bb821f9e36ef0e06dbf858d
Mít tờ HÒA <hoaduong.hutech@gmail.com>
2026-08-17 20:34:49 +0700

    ﻿0.7.0: khai cuoc, 39 bai chien thuat, luat cam chieu mai
    - Ban web 0.7.0+11
    - Trang gioi thieu: them the Khai cuoc va the 39 bai chien thuat; sua
      muc luat - cam chieu mai DA CAI, cam duoi mai thi chua

--- choi-thu/flutter_service_worker.js
+++ choi-thu/flutter_service_worker.js
@@ -3,19 +3,21 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "418c94e05fc61f229239659c17edff0e",
-"assets/AssetManifest.bin.json": "510d02e0ed499b3231d0221c07dc6d78",
-"assets/AssetManifest.json": "75987a8f09d953dd816a9159a58ddd68",
+const RESOURCES = {"assets/AssetManifest.bin": "0e70995aed458f2b90e663790c2f0317",
+"assets/AssetManifest.bin.json": "59456f1f25ef9dca964100f2283e0a99",
+"assets/AssetManifest.json": "e55903e901af41eb2e22340904943775",
 "assets/assets/am-thanh/an.wav": "bf7e38d4f6f505387af2c53de32f828e",
 "assets/assets/am-thanh/chieu.wav": "a14cc0c02446422a2b3b5eeb2a4debee",
 "assets/assets/am-thanh/di.wav": "5e116dcdbbc7806e15bbb3e77bb413d8",
 "assets/assets/am-thanh/ket-van.wav": "1dd9dfe6113a9153d54ae1fe54dbf70e",
+"assets/assets/co-the/chien-thuat.txt": "c165d97a04a25274b5dd19e584c2fe5f",
 "assets/assets/co-the/co-the.txt": "d38db0b9f0371744a337aa69411af933",
 "assets/assets/fonts/LICENSE-Apache-2.0.txt": "3b83ef96387f14655fc854ddc3c6bd57",
 "assets/assets/fonts/XiangqiHan.ttf": "fcce61ac413cf2d0a64349b91f33942a",
+"assets/assets/khai-cuoc/khai-cuoc.txt": "af346f7722b5e0f0c4d6c991b0e98de9",
 "assets/assets/thuat-ngu/thuat-ngu.txt": "c33cd8ed5ed1d69a329769cb02f88c91",
 "assets/FontManifest.json": "d09d21c39c630ade5104b699154752ea",
-"assets/fonts/MaterialIcons-Regular.otf": "879a684de73ce2b0af987c49cdeb67bd",
+"assets/fonts/MaterialIcons-Regular.otf": "247b6166e1b7af98f42af71e76c3b4a5",
 "assets/NOTICES": "7a1185c4783603f44c12e41d4c58fbdf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -30,16 +32,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "418c94e05fc61f229239659c17edff0e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "aac761790300e929efd412f02d22928f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "347dc5e06b1b312c4fa7f0aa7caa7de6",
+"flutter_bootstrap.js": "61c715b002abe567b47bed920b6ed7fe",
 "icons/Icon-192.png": "150dde07b2634d491c0cf4fd04bcef32",
 "icons/Icon-512.png": "3fe9ff80c14b1e83caac69fe1024744d",
 "icons/Icon-maskable-192.png": "1b7d8a09b3563de3ea7d2d1ec1b38684",
 "icons/Icon-maskable-512.png": "fa9a301a89295a22d4a6654e5eccbd70",
 "index.html": "bc8c5fe08bbdeb46c5a64b6d3e03fc48",
 "/": "bc8c5fe08bbdeb46c5a64b6d3e03fc48",
-"main.dart.js": "0c52114e30eb4972f3cc96918efeebcb",
+"main.dart.js": "b5d1700a760569478207d5afbdfe9806",
 "manifest.json": "c75351b95871bbda670a3b3a42e58403",
-"version.json": "71a3e4a8f2d6baecf6ae4d3843a8e5c4"};
+"version.json": "f53b37d22febead3bb3aac84fa3f702a"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
